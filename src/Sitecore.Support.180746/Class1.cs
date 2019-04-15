dc252841055bd7790ee0e60999954fb410fc53a2
Paul Kravchenko <pk@sitecore.net>
2019-04-15 13:16:12 +0300

    OnRoleModified and OnRoleAdded handlers changed to just clear roles cache. DEBUG logging was added to handlers

--- src/Sitecore.Support.180746/Class1.cs
+++ src/Sitecore.Support.180746/Class1.cs
@@ -1,49 +1,71 @@
-﻿namespace LightLDAP.Support
+﻿using System.Security.Principal;
+using LightLDAP.Caching;
+using Sitecore.Diagnostics;
+using Sitecore.StringExtensions;
+
+namespace LightLDAP.Support
 {
-  using System;
-  using System.Collections.Specialized;
-  using System.Reflection;
-  using LightLDAP.DataSource;
-  using LightLDAP.Notification;
-  using LightLDAP.Notification.Events;
+    using System;
+    using System.Collections.Specialized;
+    using System.Reflection;
+    using LightLDAP.DataSource;
+    using LightLDAP.Notification;
+    using LightLDAP.Notification.Events;
 
-  public class SitecoreADRoleProvider : LightLDAP.SitecoreADRoleProvider
-  {
-    public override void Initialize(string name, NameValueCollection config)
+    public class SitecoreADRoleProvider : LightLDAP.SitecoreADRoleProvider
     {
-      base.Initialize(name, config);
+        private RolesCache rolesCache;
 
-      // get initialized DirectoryNotificationProvider instance
-      MembershipResolver resolver = (MembershipResolver)this.GetType().BaseType.GetField("resolver", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(this);
-      PartialDataSource partialDatasource = resolver.Source;
-      DirectoryNotificationProvider notificationProvider = (DirectoryNotificationProvider)partialDatasource.GetType().GetField("notificationProvider", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(partialDatasource);
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+
+            // get initialized DirectoryNotificationProvider instance
+            MembershipResolver resolver = (MembershipResolver) this.GetType().BaseType
+                .GetField("resolver", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(this);
+            rolesCache =
+                this.GetType().BaseType.GetField("cache", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetValue(this) as RolesCache;
+
+            PartialDataSource partialDatasource = resolver.Source;
+            DirectoryNotificationProvider notificationProvider = (DirectoryNotificationProvider) partialDatasource
+                .GetType().GetField("notificationProvider", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(partialDatasource);
 
 
-      if (notificationProvider != null)
-      {
-        // cleanup the value of the ObjectCategoryAdded event and set the custom handler which performs the check if a role exists 
-        notificationProvider.GetType().GetField("ObjectCategoryAdded", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(notificationProvider, null);
-        notificationProvider.ObjectCategoryAdded += new EventHandler<ObjectCategoryAddedEventArgs>(this.OnRoleAddedWithCheck);
+            if (notificationProvider != null)
+            {
+                // cleanup the value of the ObjectCategoryAdded event and set the custom handler which performs the check if a role exists 
+                notificationProvider.ObjectCategoryAdded +=
+                    new EventHandler<ObjectCategoryAddedEventArgs>(this.OnRoleAdded);
+                notificationProvider.ObjectCategoryModified +=
+                    new EventHandler<ObjectCategoryModifiedEventArgs>(this.OnRoleModified);
+            }
+        }
 
+        private void OnRoleModified(object sender, ObjectCategoryModifiedEventArgs e)
+        {
+            Log.Debug(
+                "DIAGNOSTICS\n\tOnRoleModified. Object Name:{0}\n\tGroupType:{1}\n\tFullOUEntryPath:{2}".FormatWith(e.UserName,
+                    e.GroupType, e.FullOUEntryPath), this);
+            if (!string.IsNullOrEmpty(e.GroupType))
+            {
+                this.rolesCache.Clear();
+            }
+        }
 
-        // set patched DirectoryNotificationProvider instance back.
-        partialDatasource.GetType().GetField("notificationProvider", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(partialDatasource, notificationProvider);
-        resolver.GetType().GetProperty("Source").SetValue(resolver, partialDatasource);
-        this.GetType().BaseType.GetField("resolver", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(this, resolver);
-      }
+        // the method checks if the role exists within the scope defined by custom filter and if yes invokes the original one to add the role to the Role cache.
+        private void OnRoleAdded(object sender, ObjectCategoryAddedEventArgs e)
+        {
+            Log.Debug(
+                "DIAGNOSTICS\n\tOnRoleAdded. Object Name:{0}\n\tGroupType:{1}\n\tFullOUEntryPath:{2}".FormatWith(e.UserName,
+                    e.GroupType, e.FullOUEntryPath), this);
+            if (!string.IsNullOrEmpty(e.GroupType))
+            {
+                this.rolesCache.Clear();
+            }
+        }
+
     }
-
-    // the method checks if the role exists within the scope defined by custom filter and if yes invokes the original one to add the role to the Role cache.
-    private void OnRoleAddedWithCheck(object sender, ObjectCategoryAddedEventArgs e)
-    {
-      if (RoleExists(e.UserName))
-      {
-        MethodInfo mI = this.GetType().BaseType.GetMethod("OnRoleAdded", BindingFlags.NonPublic | BindingFlags.Instance);
-        object[] param = new[] { sender, e };
-        mI.Invoke(this, param);
-      }
-    }
-
-  }
 }
 
